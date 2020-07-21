8c53c2399a80eaca87ea774501d59216d52cf739
slorello89 <slorello89@gmail.com>
2020-07-21 13:56:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Project Name
+
+<img src="https://developer.nexmo.com/assets/images/Vonage_Nexmo.svg" height="48px" alt="Nexmo is now known as Vonage" />
+
+<!-- Add a paragraph about the project. What does it do? Who is it for? Is it actively supported? Your reader just clicked on a random link from another web page and has no idea what Nexmo is ... -->
+
+## Welcome to Nexmo
+
+<!-- change "github-repo" at the end of the link to be the name of your repo, this helps us understand which projects are driving signups so we can do more stuff that developers love -->
+
+If you're new to Nexmo, you can [sign up for a Nexmo account](https://dashboard.nexmo.com/sign-up?utm_source=DEV_REL&utm_medium=github&utm_campaign=github-repo) and get some free credit to get you started.
+
+This application assumes that you have a Vonage API Account, and an application configured to receive calls on a Vonage API https://developer.nexmo.com/concepts/guides/webhooks
+
+## Configuring the App
+
+Configure the app by editing the `VonageVoicemail/appsettings.json` file and replacing the `APP_ID` and `PRIVATE_KEY_PATH` fields with your application ID and a path to your private key.
+
+Run the app with `dotnet run`
+
+## Getting Help
+
+We love to hear from you so if you have questions, comments or find a bug in the project, let us know! You can either:
+
+* Open an issue on this repository
+* Tweet at us! We're [@NexmoDev on Twitter](https://twitter.com/NexmoDev)
+* Or [join the Nexmo Community Slack](https://developer.nexmo.com/community/slack)
+
+## Further Reading
+
+* Check out the Developer Documentation at <https://developer.nexmo.com>
+
+<!-- add links to the api reference, other documentation, related blog posts, whatever someone who has read this far might find interesting :) -->
---
+++ VonageVoicemail/Controllers/VoiceController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Nexmo.Api.Request;
+using Nexmo.Api.Voice;
+using Nexmo.Api.Voice.EventWebhooks;
+using Nexmo.Api.Voice.Nccos;
+
+namespace VonageVoicemail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoiceController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public VoiceController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("webhooks/answer")]
+        public string Answer()
+        {
+            var host = Request.Host.ToString();
+            //Uncomment the next line if using ngrok with --host-header option
+            //host = Request.Headers["X-Original-Host"];
+            var sitebase = $"{Request.Scheme}://{host}";
+
+            var talkAction = new TalkAction
+            {
+                Text = "Hello, you have reached Steve's number," +
+                " he is cannot come to the phone right now. " +
+                "Please leave a message after the tone.",
+                VoiceName = "Joey"
+            };
+
+            var recordAction = new RecordAction
+            {
+                EndOnSilence = "3",
+                BeepStart = "true",
+                EventUrl = new[] { $"{sitebase}/webhooks/recording" },
+                EventMethod = "POST"
+            };
+
+            var ncco = new Ncco(talkAction, recordAction);
+            return ncco.ToString();
+        }
+
+        [HttpPost]
+        [Route("webhooks/recording")]
+        public IActionResult Recording()
+        {
+            Record record;
+            var appId = _config["APP_ID"];
+            var privateKeyPath = _config["PRIVATE_KEY_PATH"];
+            var credentials = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKeyPath);
+            var voiceClient = new VoiceClient(credentials);
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                record = JsonConvert.DeserializeObject<Record>(reader.ReadToEndAsync().Result);
+                var recording = voiceClient.GetRecording(record.RecordingUrl);
+                System.IO.File.WriteAllBytes("your_recording.mp3", recording.ResultStream);
+            }
+
+            Console.WriteLine($"Record event received on webhook - URL: {record?.RecordingUrl}");
+            return StatusCode(204);
+        }
+    }
+}
